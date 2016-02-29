fd6750919c94ac5553952612f790e77390b53086
tunnckoCore <mameto_100@mail.bg>
2016-02-29 07:57:38 +0200

    lint, upgrade to latest idea

--- index.js
+++ index.js
@@ -7,27 +7,71 @@
 
 'use strict';
 
-module.exports = function(prefix) {
-  var debug = require('debug');
+/**
+ * Plugin that adds `debug` method to the instance
+ * and `namespaces` are added to this method.
+ *
+ * ```js
+ * app.debug()('debugging on main namespace')
+ * //=> base debugging on main namespace
+ *
+ * app.debug.one = app.debug('one')
+ * app.debug.one('testing %s, whatever', 'foo, bar')
+ * //=> base:one testing foo, bar, whatever
+ *
+ * app.debug.mix = app.debug('two', 123 'three:four', 'five')
+ * app.debug.mix('okey, %s awesome', 'this is')
+ * //=> base:two:three:four:five okey, this is awesome
+ *
+ * app.debug.helper('is one of internal namespaces added')
+ * //=> base:helper is one of internal namespaces added
+ * ```
+ *
+ * @name   debug
+ * @param  {Array} `namespaces`
+ * @return {Function}
+ */
 
-  return function(app) {
-    this.define('debug', function(name) {
-      var fn = debug(namespace(prefix, name));
-      return function() {
-        return fn.apply(debug, arguments);
-      };
+module.exports = function debugPlugin(namespaces) {
+  namespaces = Array.isArray(namespaces) ? namespaces : [namespaces];
+
+  return function plugin(app) {
+    app.define('_debugNamespace', app._namespace);
+    app.define('debug', function debug() {
+      return debugFactory.apply(app, arguments);
     });
+    if (namespaces.length) {
+      var len = namespaces.length;
+      var i = 0;
+
+      while (i < len) {
+        var ns = namespaces[i++];
+        app.debug[ns] = debugFactory.call(app, ns);
+        app.debug[ns].color = app.debug.color;
+      }
+    }
   };
 };
 
-function namespace(prefix, subname) {
-  var args = [].slice.call(arguments).filter(Boolean);
-  var name = 'base';
-  if (args.length === 1) {
-    name += ':' + args[0];
+function debugFactory() {
+  var app = this;
+  var args = [].slice.call(arguments);
+  var segs = app._namespace.split(':');
+  var len = args.length;
+  var i = 0;
+
+  while (i < len) {
+    var val = args[i++];
+    if (typeof val === 'string') {
+      segs.push.apply(segs, val.split(':'));
+    }
   }
-  if (args.length === 2) {
-    name += ':' + args.join(':');
-  }
-  return name;
+
+  return function debug() {
+    var fn = require('debug');
+    var namespace = segs.join(':');
+    app.define('_debugNamespace', namespace);
+    fn(namespace).apply(fn, arguments);
+    return app;
+  };
 }
--- test.js
+++ test.js
@@ -1,15 +1,14 @@
 'use strict';
 
-require('mocha');
 var assert = require('assert');
-var debug = require('./');
+var debug = require('./index');
 var Base = require('base');
-var app;
+var app = null;
 
 describe('base-debug', function() {
   beforeEach(function() {
     app = new Base();
-    app.use(debug('foo'));
+    app.use(debug());
   });
 
   it('should export a function', function() {
@@ -27,5 +26,44 @@ describe('base-debug', function() {
   it('should take a namespace', function() {
     var debug = app.debug('bar');
     debug('debugging %s', 'whatever');
+    assert.equal(app._namespace, 'base');
+    assert.equal(app._debugNamespace, 'base:bar');
+  });
+
+  it('should take a complex namespace', function() {
+    app.debug('bar:one', 123, 'foo:qux:two', 'four', 'five:six')('hello world');
+    //=> base:bar:one:foo:qux:two:four:five:six hello world
+
+    assert.equal(app._namespace, 'base');
+    assert.equal(app._debugNamespace, 'base:bar:one:foo:qux:two:four:five:six');
+  });
+
+  it('should not have built-in debug namespaces', function() {
+    app.debug('two')('wooo hooo');
+    //=> base:two wooo hooo
+
+    assert.equal(typeof app.debug.context, 'undefined');
+    assert.equal(typeof app.debug.engine, 'undefined');
+    assert.equal(typeof app.debug.helper, 'undefined');
+    assert.equal(app._debugNamespace, 'base:two');
+  });
+
+  it('should be able to pass custom namespaces', function() {
+    var base = new Base();
+    base.use(debug(['helper', 'engine']));
+    base.debug.helper('loading foo helper');
+    //=> base:helper loading foo helper
+
+    assert.equal(base._debugNamespace, 'base:helper');
+    assert.equal(typeof base.debug.engine, 'function');
+  });
+
+  it('should create new namespace with `app.debug()`', function() {
+    var base = new Base();
+    base.use(debug());
+
+    base.debug.item = base.debug('collection:item');
+    base.debug.item('loading collection item');
+    //=> base:collection:item loading collection item
   });
 });
